9ac3606fb106346ad95c1cbce00ab85af0c9ac7b
dongtais <dongtaishi.w@gmail.com>
2024-07-27 13:41:01 -0400

    add todo

--- Character.cc
+++ Character.cc
@@ -6,7 +6,7 @@ int Character::receiveAttack(int attackerAtk)
 {
    int takenDamage = std::ceil(100.0 / (100.0 + baseDef)) * attackerAtk;
    hp -= takenDamage;
-   std::cout << "Attacker atk: " << attackerAtk << "Took damage: " << takenDamage << " Remaining hp: " << hp << std::endl;
+   std::cout << "Attacker atk: " << attackerAtk << "Took damage: " << takenDamage << " Remaining hp: " << hp << std::endl; // TODO: Remove this
    if (hp <= 0)
    {
       onDeath();
