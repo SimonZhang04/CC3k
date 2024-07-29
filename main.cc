61093ff8348698ea73ef74de1d143cf94b614a93
dongtais <dongtaishi.w@gmail.com>
2024-07-29 18:25:09 -0400

    add -f flag for floorfile, -s flag for seed

--- main.cc
+++ main.cc
@@ -1,5 +1,15 @@
 #include "GameLogic.h"
 
+char *getOption(char **begin, char **end, const std::string &flag)
+{
+    char **iterator = std::find(begin, end, flag);
+    if (iterator != end && ++iterator != end)
+    {
+        return *iterator;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     GameLogic gl;
@@ -7,9 +17,18 @@ int main(int argc, char *argv[])
     std::string floorFile = "";
     if (argc > 1)
     {
-        floorFile = argv[1];
-    }
+        char *map = getOption(argv, argv + argc, "-f");
+        if (map)
+        {
+            floorFile = map;
+        }
 
+        char *seed = getOption(argv, argv + argc, "-s");
+        if (seed)
+        {
+            srand(std::stoi(seed));
+        }
+    }
     gl.playGame(floorFile);
     return 0;
 }
